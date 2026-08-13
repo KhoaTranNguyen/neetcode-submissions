ec673688a9024a9ed1beae6ebe272a06ad2c1888
Khoa Tran Nguyen <trannguyenkhoa2005@gmail.com>
2026-08-13 13:07:48 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.rs
@@ -0,0 +1,20 @@
+use std::collections::BTreeMap;
+
+impl Solution {
+    
+    pub fn is_anagram(s: String, t: String) -> bool {
+
+        if s.len() != t.len() {
+            return false;
+        }
+        
+        let mut counts: HashMap<char, [usize; 2]> = HashMap::new();
+
+        for (ch_s, ch_t) in s.chars().zip(t.chars()) {
+            counts.entry(ch_s).or_insert([0, 0])[0] += 1;
+            counts.entry(ch_t).or_insert([0, 0])[1] += 1;
+        }
+
+        counts.values().all(|[c1, c2]| c1 == c2)
+    }
+}
