0dc36b98382327cb86ba950e28e8b08f8f1c8bb6
Khoa Tran Nguyen <trannguyenkhoa2005@gmail.com>
2026-08-13 13:44:45 +0200

    Add: is-anagram - submission-28

---
+++ Data Structures & Algorithms/is-anagram/submission-28.rs
@@ -0,0 +1,22 @@
+use std::collections::BTreeMap;
+
+impl Solution {
+    
+    pub fn is_anagram(s: String, t: String) -> bool {
+
+        if s.len() != t.len() {
+            return false;
+        }
+
+        // Stack-allocated 52-byte arrays (Zero Heap Overhead!)
+        let mut s_arr = [0u16; 26];
+        let mut t_arr = [0u16; 26];
+
+        for (a, b) in s.bytes().zip(t.bytes()) {
+            s_arr[(a - b'a') as usize] += 1;
+            t_arr[(b - b'a') as usize] += 1;
+        }
+
+        s_arr == t_arr // 1-instruction SIMD vector compare
+    }
+}
